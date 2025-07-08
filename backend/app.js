3b180dad9f7ee836da55271aef38ea121a5a8c4b
chaitu-169 <sriramchaitu383@gmail.com>
2025-07-08 16:14:03 +0530

    Remove node_modules from repo

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    origin: process.env.FRONTEND_URL.split(","), 
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
